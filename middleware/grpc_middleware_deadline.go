1ac2f42e9e699692b38eb5177b6acbc570f6fa56
fellowme <1928470314@qq.com>
2021-07-14 09:54:42 +0800

    Update grpc_middleware_deadline.go

--- middleware/grpc_middleware_deadline.go
+++ middleware/grpc_middleware_deadline.go
@@ -1,15 +1,16 @@
-package middleware
+package deadline
 
 import (
 	"context"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
+	"grpc-gorm/tools/logSetup"
 )
 
 func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		if ctx.Err() == context.Canceled {
-			MyLogger.Error("请求超时")
+			logSetup.MyLogger.Error("请求超时")
 			return nil, errors.New("超时取消")
 		}
 		resp, err := handler(ctx, req)
